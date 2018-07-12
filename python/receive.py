cf05739fc3723184d11cfb534d9834464e1df5b9
Victor Biga <victor.biga@gmail.com>
2018-07-13 00:39:49 +0100

    another code

---
+++ python/receive.py
@@ -0,0 +1,9 @@
+#Problem : receive miles and convert to kilometerss
+m_km = input('Enter how many miles, preferably 5 ')
+m_km = int(m_km)
+#kilometres = miles * 1.60934
+km = m_km * 1.60934
+#Enter Miles 5
+#5 miles equals 8.04
+print("{} miles equals {}".format(m_km, km))
+
